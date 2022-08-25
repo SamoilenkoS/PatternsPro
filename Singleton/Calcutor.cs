f63e0fa9d411f97589e29c39c7c411a1398c51c4
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2022-08-25 21:41:59 +0300

    Add project files.

---
+++ Singleton/Calcutor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Calcutor
+    {
+        private static Calcutor _instance;
+
+        private Calcutor()
+        {
+
+        }
+
+        public static Calcutor Instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    _instance = new Calcutor();
+                }
+
+                return _instance;
+            }
+        }
+
+        public int Add(int a, int b) => a + b;
+        public int Substract(int a, int b) => a - b;
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculator = Calcutor.Instance;
+
+            var calc = Calcutor.Instance;
+            Console.WriteLine(calculator.Add(10, 20));
+        }
+    }
+}
